36f4ec904e6a5a135f90ca8665aadaaed049143d
m990m1 <m990m1@126.com>
2025-06-10 15:47:09 +0800

    adjust code

--- src/sort.py
+++ src/sort.py
@@ -15,3 +15,25 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
+
+def quick_sort(arr):
+    """
+    Perform quick sort on a list.
+
+    :param arr: List of elements to be sorted.
+    :return: Sorted list.
+    """
+    if len(arr) <= 1:
+        return arr
+    
+    # 选择基准元素
+    pivot = arr[len(arr) // 2]
+    
+    # 分割数组
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    
+    # 递归排序并合并结果
+    return quick_sort(left) + middle + quick_sort(right)
